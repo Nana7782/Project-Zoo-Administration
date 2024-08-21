c3d9d388eee82d38637338d5da7422b8482df152
TOezdemir <tolgay.oezdemir@hsrw.org>
2024-08-21 10:16:59 +0200

    interace set

---
+++ src/interfaces/IAnimal.ts
@@ -0,0 +1,10 @@
+interface IAnimal {
+  emoji: string;
+  name: string;
+  yearOfBirth: number;
+  continents: string[];
+  specialNeeds: string[];
+  enclosureId: Enclosure;
+}
+
+export default IAnimal;
